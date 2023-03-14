7a979a1c2d6da1974243241ac5d4b67cec3de897
Tren-123 <pavel.filatov1@yandex.ru>
2023-03-14 20:28:21 +0300

    Add preview of images in admin panel

--- places/admin.py
+++ places/admin.py
@@ -3,10 +3,25 @@ from django.contrib.gis.db import models as geomodels
 from django import forms
 from .models import Place, Image
 from .forms import PlaceGeoInputForm
+from django.utils.html import format_html
 
 class ImageInline(admin.TabularInline):
     model = Image
-    fields = ('image', 'number')
+    readonly_fields = ["preview_image"]
+
+    def preview_image(self, obj):
+        if obj.image.height > 200:
+            preview_width = int(obj.image.width/(obj.image.height/200))
+            preview_height = 200
+        else:
+            preview_width = obj.image.width
+            preview_height = obj.image.height
+        return format_html('<img src="{url}" width="{width}" height={height} />',
+            url = obj.image.url,
+            width = preview_width,
+            height = preview_height,
+                )
+    fields = ('image', 'preview_image', 'number')
 
 class PlaceAdmin(admin.ModelAdmin):
     formfield_overrides = {
